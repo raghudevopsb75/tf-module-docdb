3f6b7d81c7f5db84ba3f87f640daa0c5a3bf9d02
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2023-11-20 07:43:09 +0530

    [D75-7] | Raghu K | Terraform Code for RoboShop Project.

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "aws_docdb_subnet_group" "main" {
   }
 }
 
-resource "aws_docdb_cluster" "docdb" {
+resource "aws_docdb_cluster" "main" {
   cluster_identifier   = "${var.env}-${var.component}-cluster"
   master_username      = data.aws_ssm_parameter.master_username.value
   master_password      = data.aws_ssm_parameter.master_password.value
@@ -43,10 +43,10 @@ resource "aws_docdb_cluster" "docdb" {
   kms_key_id           = var.kms_key_id
 }
 
-resource "aws_docdb_cluster_instance" "cluster_instances" {
+resource "aws_docdb_cluster_instance" "main" {
   count              = var.docdb_instance_count
   identifier         = "${var.env}-${var.component}-${count.index}"
-  cluster_identifier = aws_docdb_cluster.docdb.id
+  cluster_identifier = aws_docdb_cluster.main.id
   instance_class     = var.docdb_instance_class
 }
 
